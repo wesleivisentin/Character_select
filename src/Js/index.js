6401930d08141486027b2060aaef3547394e1e80
weslei <wesleivisentin@hotmail.com>
2022-05-22 18:40:43 -0300

    2

---
+++ src/Js/index.js
@@ -0,0 +1,12 @@
+/* OBJETIVO 1 - quando passar o mouse em cima do personagem temo que:
+
+ - colocar a classe selecionado no personagem que passamos o mouse em cima para adicionar a animaçao nele
+
+- retirar a classe selecionado do personagem que esta selecionado
+
+
+OBJETIVO 2 - quando passar o mouse em cima do personagem, trocar a imagem e nome do personagem grande
+
+- aletrar a imagem do personagem jogador 1
+- alterar o nome do porsonagem selecionado jogador 1
+ */
