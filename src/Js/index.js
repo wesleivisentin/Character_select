3fe5ae99a9dbbc6eac5b22fd3cab54945bebe2c6
weslei <wesleivisentin@hotmail.com>
2022-05-22 20:54:54 -0300

    Update index.js

--- src/Js/index.js
+++ src/Js/index.js
@@ -14,4 +14,23 @@ https://www.youtube.com/watch?v=8g-9zSafztw&t=6719s
  */
 
 
-const homemDeFerro = 
+// const homemDeFerro = document.getElementById("homem-de-ferro")
+
+// homemDeFerro.addEventListener("mouseenter", () => { 
+//     homemDeFerro.classList.add("selecionado")
+// })
+
+const personagens = document.querySelectorAll(".personagem")
+
+personagens.forEach((personagem) => {
+    personagem.addEventListener("mouseenter", () => {
+        const personagemSelecionado = document.querySelector(".selecionado")
+        personagemSelecionado.classList.remove("selecionado")
+
+        personagem.classList.add("selecionado")
+    })
+})
+
+
+
+
