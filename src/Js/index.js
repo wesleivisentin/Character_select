980c31a05ad981689c71800ee0e04b0c6289e5e1
weslei <wesleivisentin@hotmail.com>
2022-05-23 20:46:16 -0300

    2

--- src/Js/index.js
+++ src/Js/index.js
@@ -24,10 +24,33 @@ const personagens = document.querySelectorAll(".personagem")
 
 personagens.forEach((personagem) => {
     personagem.addEventListener("mouseenter", () => {
+
+        //////////////////////////////////////////
+        const idSelecionado = personagem.attributes.id.value
+
+        if(idSelecionado === "ultron"){
+            return
+
+        }
+
+        ///////////////////////////////////////////
         const personagemSelecionado = document.querySelector(".selecionado")
         personagemSelecionado.classList.remove("selecionado")
 
         personagem.classList.add("selecionado")
+
+
+
+        //OBEJETIVO 2
+       
+        
+        const imagemJogador1 = document.getElementById("personagem-jogador-1")
+        imagemJogador1.src = `./src/imagens/${idSelecionado}.png`
+
+        const nomeJogador1 = document.getElementById("nome-jogador-1")
+        const nomeSelecionado = personagem.getAttribute('data-name')
+
+        nomeJogador1.innerHTML = nomeSelecionado
     })
 })
 
